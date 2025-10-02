6bc8a75edb88d0a706618dade5b8e77d417fb162
Daffa964 <daffafairuz57@gmail.com>
2025-10-02 11:52:34 +0700

    edit-surat

--- sistem-surat-desa/resources/views/layout.blade.php
+++ sistem-surat-desa/resources/views/layout.blade.php
@@ -1,215 +1,163 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $pengajuan->jenisSurat->nama_surat }}</title>
     <style>
+        @page {
+            /* Set margin halaman langsung di sini */
+            margin: 2cm 1.5cm;
+        }
+
         body {
-            font-family: "Times New Roman", Times, serif;
+            font-family: 'Times New Roman', Times, serif;
             font-size: 12pt;
-            line-height: 1.6;
+            line-height: 1.5;
             margin: 0;
             padding: 0;
         }
-        
+
+        /* Container tidak perlu padding/width lagi karena sudah diatur di @page */
         .container {
-            width: 21cm;
-            min-height: 29.7cm;
-            margin: 0 auto;
-            padding: 2cm;
-            box-sizing: border-box;
+            width: 100%;
+            position: relative;
         }
-        
-        .header {
-            text-align: center;
-            margin-bottom: 1cm;
-            border-bottom: 3px double #000;
-            padding-bottom: 10px;
-        }
-        
-        .logo-container {
-            position: absolute;
-            left: 2cm;
-            top: 2cm;
-        }
-        
-        .logo {
-            width: 2.5cm;
-            height: 3cm;
-            border: 1px solid #000;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            font-size: 8pt;
-            text-align: center;
-        }
-        
+
         .kop-surat {
-            text-align: center;
-            line-height: 1.2;
+            width: 100%;
+            border-bottom: 4px double #000;
+            padding-bottom: 15px;
+            margin-bottom: 25px;
         }
-        
-        .kop-surat .institution {
-            font-size: 14pt;
+
+        .kop-surat .logo {
+            width: 85px;
+            /* Sesuaikan ukuran logo jika perlu */
+            height: auto;
+        }
+
+        .kop-surat .text-kop {
+            text-align: center;
+            line-height: 1.3;
+        }
+
+        .kop-surat .text-kop .nama-instansi {
+            font-size: 18pt;
             font-weight: bold;
         }
-        
-        .kop-surat .institution-detail {
-            font-size: 12pt;
+
+        .kop-surat .text-kop .nama-detail {
+            font-size: 16pt;
             font-weight: bold;
         }
-        
-        .kop-surat .alamat {
-            font-size: 10pt;
+
+        .kop-surat .text-kop .alamat {
+            font-size: 11pt;
         }
-        
-        .kop-surat .contact {
-            font-size: 10pt;
-        }
-        
-        .nomor-surat {
+
+        /* Mengganti class .perihal dan .nomor-surat */
+        .judul-surat {
             text-align: center;
-            margin: 1cm 0;
-            padding-top: 5px;
-        }
-        
-        .nomor-surat p {
-            margin: 0;
+            font-weight: bold;
             text-decoration: underline;
+            font-size: 14pt;
+            margin-top: 20px;
+            margin-bottom: 5px;
         }
-        
-        .perihal {
+
+        .nomor-surat-text {
             text-align: center;
-            font-weight: bold;
-            margin-bottom: 1cm;
+            font-size: 12pt;
+            margin-bottom: 30px;
         }
-        
+
         .content {
             text-align: justify;
-            margin-bottom: 1cm;
         }
-        
+
         .content p {
             margin: 0 0 1em 0;
-            text-indent: 2cm;
+            text-indent: 2.5cm;
         }
-        
+
         .content .no-indent {
             text-indent: 0;
         }
-        
+
         .data-table {
             width: 100%;
-            margin: 0.5cm 0;
+            margin-left: 2.5cm;
+            /* Menjorok ke dalam */
             border-collapse: collapse;
         }
-        
+
         .data-table td {
-            padding: 2px 0;
+            padding: 4px 0;
+            vertical-align: top;
         }
-        
+
         .tanda-tangan {
-            width: 8cm;
+            width: 40%;
             float: right;
             text-align: center;
-            margin-top: 1cm;
+            margin-top: 40px;
         }
-        
+
         .tanda-tangan .jabatan {
-            margin-bottom: 2cm;
+            /* Kurangi jarak bawah untuk memberi ruang bagi QR Code */
+            margin-bottom: 10px;
         }
-        
+
+        .tanda-tangan .qr-code {
+            /* Memberi sedikit spasi dan memastikan di tengah */
+            margin: 5px auto;
+            display: block;
+        }
+
+        .tanda-tangan .qr-code svg {
+            width: 80px;
+            /* Ukuran QR Code */
+            height: 80px;
+        }
+
         .tanda-tangan .nama {
             font-weight: bold;
             text-decoration: underline;
-            margin-top: 1cm;
+            /* Beri jarak dari QR Code */
+            margin-top: 10px;
         }
-        
-        .qr-container {
-            position: absolute;
-            right: 2cm;
-            bottom: 3cm;
-            text-align: center;
-        }
-        
-        .qr-code {
-            margin: 0 auto;
-        }
-        
-        .qr-code svg {
-            width: 3cm;
-            height: 3cm;
-        }
-        
-        .qr-note {
-            font-size: 10pt;
-            margin-top: 0.5cm;
-        }
-        
-        .footer {
-            position: absolute;
-            bottom: 2cm;
-            width: 100%;
-            text-align: center;
-        }
-        
-        .footer p {
-            margin: 0;
-            font-size: 10pt;
-        }
-        
-        .stamp {
-            position: absolute;
-            right: 5cm;
-            bottom: 4cm;
-            width: 3cm;
-            height: 3cm;
-            border: 2px dashed #999;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            font-size: 8pt;
-            text-align: center;
-        }
-        
+
         .clear {
             clear: both;
         }
     </style>
 </head>
+
 <body>
     <div class="container">
-        <div class="logo-container">
-            <div class="logo">Logo Desa</div>
-        </div>
-        
-        <div class="header">
-            <div class="kop-surat">
-                <div class="institution">PEMERINTAH KABUPATEN SEJAHTERA</div>
-                <div class="institution-detail">KECAMATAN MAJU JAYA</div>
-                <div class="institution-detail">DESA KEMAKMURAN</div>
-                <div class="alamat">Alamat: Jalan Merdeka No. 1 Desa Kemakmuran, Kecamatan Maju Jaya</div>
-                <div class="contact">Telepon: (021) 123456 | Email: desakemakmuran@example.com</div>
-            </div>
-        </div>
-        
+        <table class="kop-surat">
+            <tr>
+                <td style="width: 20%; text-align: center;">
+                    <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('logo-desa.png'))) }}"
+                        alt="Logo Desa" class="logo">
+                </td>
+                <td style="width: 80%;" class="text-kop">
+                    <div class="nama-instansi">PEMERINTAH KABUPATEN SEJAHTERA</div>
+                    <div class="nama-detail">KECAMATAN MAJU JAYA</div>
+                    <div class="nama-detail">DESA KEMAKMURAN</div>
+                    <div class="alamat">Alamat: Jalan Merdeka No. 1 Desa Kemakmuran, Kecamatan Maju Jaya</div>
+                    <div class="alamat">Telepon: (021) 123456 | Email: desakemakmuran@example.com</div>
+                </td>
+            </tr>
+        </table>
+
         @yield('content')
-        
+
         <div class="clear"></div>
-        
-        <div class="stamp">
-            Cap Desa
-        </div>
-        
-        <div class="qr-container">
-            <div class="qr-code">{!! $qrCode !!}</div>
-            <div class="qr-note">Scan QR Code untuk verifikasi</div>
-        </div>
-        
-        <div class="footer">
-            <p>Surat ini dicetak oleh Sistem Surat Desa Digital pada {{ now()->format('d F Y H:i:s') }}</p>
-        </div>
+
     </div>
 </body>
+
 </html>
--- sistem-surat-desa/resources/views/template/sktm.blade.php
+++ sistem-surat-desa/resources/views/template/sktm.blade.php
@@ -1,75 +1,67 @@
 @extends('layout')
 
 @section('content')
-<div class="nomor-surat">
-    <p>Nomor: {{ $nomor_surat }}</p>
-</div>
+    <div class="judul-surat">
+        SURAT KETERANGAN TIDAK MAMPU
+    </div>
+    <div class="nomor-surat">
+        Nomor: {{ $nomor_surat }}
+    </div>
 
-<div class="perihal">
-    <p>SURAT KETERANGAN TIDAK MAMPU</p>
-    <p>TENTANG ORANG TUA / WALI</p>
-</div>
+    <div class="content">
+        <p class="no-indent">Yang bertanda tangan di bawah ini Kepala Desa Kemakmuran, Kecamatan Maju Jaya, Kabupaten
+            Sejahtera, dengan ini menerangkan bahwa:</p>
 
-<div class="content">
-    <p>Yang bertanda tangan di bawah ini Kepala Desa Kemakmuran, Kecamatan Maju Jaya, Kabupaten Sejahtera dengan ini menerangkan bahwa :</p>
-    
-    <table class="data-table">
-        <tr>
-            <td width="30%">Nama</td>
-            <td width="5%">:</td>
-            <td>{{ $pengajuan->user->name }}</td>
-        </tr>
-        <tr>
-            <td>NIK</td>
-            <td>:</td>
-            <td>{{ $pengajuan->user->dataWarga->nik }}</td>
-        </tr>
-        <tr>
-            <td>Tempat/Tanggal Lahir</td>
-            <td>:</td>
-            <td>{{ $pengajuan->user->dataWarga->tempat_lahir }}, 
-                @if($pengajuan->user->dataWarga->tanggal_lahir instanceof \Carbon\Carbon)
-                    {{ $pengajuan->user->dataWarga->tanggal_lahir->format('d F Y') }}
-                @elseif($pengajuan->user->dataWarga->tanggal_lahir)
-                    {{ \Carbon\Carbon::parse($pengajuan->user->dataWarga->tanggal_lahir)->format('d F Y') }}
-                @else
-                    {{ $pengajuan->user->dataWarga->tanggal_lahir }}
-                @endif
-            </td>
-        </tr>
-        <tr>
-            <td>Jenis Kelamin</td>
-            <td>:</td>
-            <td>{{ $pengajuan->user->dataWarga->jenis_kelamin }}</td>
-        </tr>
-        <tr>
-            <td>Pekerjaan</td>
-            <td>:</td>
-            <td>Wiraswasta</td>
-        </tr>
-        <tr>
-            <td>Alamat</td>
-            <td>:</td>
-            <td>{{ $pengajuan->user->dataWarga->alamat }}</td>
-        </tr>
-        <tr>
-            <td>Keperluan</td>
-            <td>:</td>
-            <td>{{ $pengajuan->data_tambahan['keperluan'] ?? '-' }}</td>
-        </tr>
-    </table>
+        <table class="data-table">
+            <tr>
+                <td style="width: 30%;">Nama</td>
+                <td style="width: 5%;">:</td>
+                <td style="width: 65%;">{{ $pengajuan->user->name }}</td>
+            </tr>
+            <tr>
+                <td>NIK</td>
+                <td>:</td>
+                <td>{{ $pengajuan->user->dataWarga->nik }}</td>
+            </tr>
+            <tr>
+                <td>Tempat/Tanggal Lahir</td>
+                <td>:</td>
+                <td>
+                    {{ $pengajuan->user->dataWarga->tempat_lahir }},
+                    {{ \Carbon\Carbon::parse($pengajuan->user->dataWarga->tanggal_lahir)->isoFormat('D MMMM Y') }}
+                </td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>:</td>
+                <td>{{ $pengajuan->user->dataWarga->jenis_kelamin }}</td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>{{ $pengajuan->user->dataWarga->alamat }}</td>
+            </tr>
+        </table>
 
-    <p class="no-indent">Benar nama tersebut di atas adalah penduduk Desa Kemakmuran dan berdasarkan keterangan dari Ketua RT/RW setempat bahwa nama tersebut di atas termasuk keluarga tidak mampu.</p>
-    
-    <p class="no-indent">Surat keterangan ini diberikan untuk keperluan : {{ $pengajuan->data_tambahan['keperluan'] ?? '-' }}.</p>
-    
-    <p class="no-indent">Demikian Surat Keterangan Tidak Mampu ini dibuat dengan sebenarnya dan untuk dapat dipergunakan sebagaimana mestinya.</p>
-</div>
+        <p>Benar bahwa yang bersangkutan adalah penduduk Desa Kemakmuran dan berdasarkan data serta pengetahuan kami, yang
+            bersangkutan termasuk dalam kategori keluarga tidak mampu.</p>
 
-<div class="tanda-tangan">
-    <p>Kemakmuran, {{ now()->format('d F Y') }}</p>
-    <p class="jabatan">Kepala Desa Kemakmuran</p>
-    <p class="nama">BUDI SANTOSO</p>
-    <p>NIP. 19750101 200001 1 001</p>
-</div>
+        <p>Surat keterangan ini dibuat untuk keperluan:
+            <strong>{{ $pengajuan->data_tambahan['keperluan'] ?? '-' }}</strong>.
+        </p>
+
+        <p>Demikian surat keterangan ini dibuat untuk dapat dipergunakan sebagaimana mestinya.</p>
+    </div>
+
+    <div class="tanda-tangan">
+        <p>Kemakmuran, {{ now()->isoFormat('D MMMM Y') }}</p>
+        <p class="jabatan">Kepala Desa Kemakmuran</p>
+
+        <div class="qr-code">
+            {!! $qrCode !!}
+        </div>
+
+        <p class="nama">BUDI SANTOSO</p>
+        <p>NIP. 19750101 200001 1 001</p>
+    </div>
 @endsection
